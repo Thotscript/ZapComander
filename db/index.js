40a0b57718d47d2b0e038caefd0e29554db8f776
Thotscript <jurasaopauloo@hotmail.com>
2025-04-17 15:51:36 -0300

    Atualizacao de pastas

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',       // ou o nome do container docker, ex: 'mysql'
+    host: 'mysql',       // ou o nome do container docker, ex: 'mysql'
     user: 'wpptalk',
     password: 'wpptalk1234',
     database: 'wpptalk_db',
