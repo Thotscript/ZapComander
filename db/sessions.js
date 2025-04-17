c6d626f35d03909c300ae70487cab11bceea0c48
Thotscript <jurasaopauloo@hotmail.com>
2025-04-17 17:27:32 -0300

    Correcao de arquivos mysql

--- db/sessions.js
+++ db/sessions.js
@@ -1,10 +1,11 @@
+// db/sessions.js
 import db from './index.js';
 
-export async function criarOuIgnorarUsuario(email) {
+export async function criarOuIgnorarSessao(numero, email) {
   const sql = `
-    INSERT INTO usuarios (email)
-    VALUES (?)
-    ON DUPLICATE KEY UPDATE email = email
+    INSERT INTO sessoes (numero, usuario_email)
+    VALUES (?, ?)
+    ON DUPLICATE KEY UPDATE numero = numero
   `;
-  await db.query(sql, [email]);
+  await db.query(sql, [numero, email]);
 }
