95130a5f10af6bc3d6848316511f355f100f1f79
Thotscript <jurasaopauloo@hotmail.com>
2025-04-25 12:54:32 -0300

    m

--- db/sessions.js
+++ db/sessions.js
@@ -1,11 +1,11 @@
 // db/sessions.js
 import db from './index.js';
 
-export async function criarOuIgnorarSessao(numero, email, profile_name) {
+export async function criarOuIgnorarSessao(numero, email) {
   const sql = `
     INSERT INTO sessoes (numero, usuario_email)
     VALUES (?, ?, ?)
     ON DUPLICATE KEY UPDATE numero = numero
   `;
-  await db.query(sql, [numero, email, profile_name]);
+  await db.query(sql, [numero, email]);
 }
