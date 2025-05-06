8a0fab9b9e518a672d27a2358044420bf5f236f0
Thotscript <jurasaopauloo@hotmail.com>
2025-05-06 14:32:17 -0300

    Ajuste de Host

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',    // conecta no seu host local
+  host: 'wpptalk_db',    // conecta no seu host local
   port: 3306,           // porta mapeada no docker-compose (3306:3306)
   user: 'wpptalk',
   password: 'wpptalk1234',
