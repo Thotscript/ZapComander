9e0fc3f9c2f6c942bc6d6715573aafc1d3080a45
Thotscript <jurasaopauloo@hotmail.com>
2025-05-19 12:11:49 -0300

    Ajuste UTF-8

--- db/index.js
+++ db/index.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
   database: 'wpptalk_db',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  charset:  'utf8mb4'
 });
 
 export default pool;
